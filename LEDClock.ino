b25879e1ddafb1da4074f593eb1c77a65e36e9ec
scorpion <scorp251@gmail.com>
2015-01-31 12:04:20 +0300

    xz

--- LEDClock.ino
+++ LEDClock.ino
@@ -11,35 +11,46 @@ void setup()
 	pinMode(clockPinBase, OUTPUT);
 	pinMode(dataPinBase, OUTPUT);
 
+	shiftOut(dataPinBase, clockPinBase, MSBFIRST, 0xff);
+
 	pinMode(latchPinArrow, OUTPUT);
 	pinMode(clockPinArrow, OUTPUT);
 	pinMode(dataPinArrow, OUTPUT);
 
 	digitalWrite(latchPinArrow, LOW);
-	for (int i = 0; i < 8; i++)
+	for (int j = 0; j < 8; j++)
 	{
-		shiftOut(dataPinArrow, clockPinArrow, MSBFIRST, 0);
+		for (int i = 0; i < 8; i++)
+		{
+			shiftOut(dataPinArrow, clockPinArrow, MSBFIRST, 0 << i);
+		}
 	}
 	digitalWrite(latchPinArrow, HIGH);
 }
 
+uint8_t globalBase = 0;
+
 void loop()
 {
-	digitalWrite(latchPinArrow, HIGH);
-	shiftOut(dataPinArrow, clockPinArrow, MSBFIRST, 0x01);
+
 	digitalWrite(latchPinArrow, LOW);
-	shiftOut(dataPinBase, clockPinBase, MSBFIRST, 0x01);
-	for (int i = 0; i < 8; i++)
+	for (int j = 0; j < 8; j++)
 	{
-		digitalWrite(latchPinArrow, LOW);
-		shiftOut(dataPinArrow, clockPinArrow, MSBFIRST, 0x00);
-		digitalWrite(latchPinArrow, LOW);
-		delay(1000);
-	}
-	digitalWrite(latchPinArrow, LOW);
-	for (int i = 0; i < 8 ; i++)
-	{
-		shiftOut(dataPinArrow, clockPinArrow, MSBFIRST, 0);
+		shiftOut(dataPinArrow, clockPinArrow, MSBFIRST, 0b10000001);
+		shiftOut(dataPinArrow, clockPinArrow, MSBFIRST, 0b10000001);
 	}
 	digitalWrite(latchPinArrow, HIGH);
+
+	delay(500);
+
+	digitalWrite(latchPinArrow, LOW);
+	for (int j = 0; j < 8; j++)
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			shiftOut(dataPinArrow, clockPinArrow, MSBFIRST, 0 << i);
+		}
+	}
+	digitalWrite(latchPinArrow, HIGH);
+	delay(500);
 }
